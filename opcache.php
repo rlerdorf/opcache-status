2a5a404e0797eca473920b0e59a9ed4f7d97c76f
James White <james.white.15@gmail.com>
2013-11-28 02:21:04 +0000

    Fixing name

--- opcache.php
+++ opcache.php
@@ -1,7 +1,7 @@
 <?php
 
 if (!extension_loaded('Zend OPcache')) {
-	echo '<div style="background-color: #F2DEDE; color: #B94A48; padding: 1em;">You do not have the Zend OPCache extension loaded, sample data is being shown instead.</div>';
+	echo '<div style="background-color: #F2DEDE; color: #B94A48; padding: 1em;">You do not have the Zend OPcache extension loaded, sample data is being shown instead.</div>';
 	require 'data-sample.php';
 }
 
