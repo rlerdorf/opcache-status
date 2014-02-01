32c6724f94b46dc3009b3c0340b8e8f872a651e7
James White <james.white.15@gmail.com>
2014-02-01 14:23:35 +0000

    Tidying and improving usability of partition visualisation

--- opcache.php
+++ opcache.php
@@ -12,6 +12,7 @@ $status = opcache_get_status();
 /**
  * Turn bytes into a human readable format
  * @param $bytes
+ * @return string The formatted size
  */
 function size_for_humans($bytes) {
 	if ($bytes > 1048576) {
@@ -23,10 +24,14 @@ function size_for_humans($bytes) {
 
 /**
  * Borrowed from Laravel
+ * @param array $array The array to set a key/value in
+ * @param mixed $key
+ * @param mixed $value
+ * @return array The array with the new key set
  */
 function array_pset(&$array, $key, $value) {
 	if (is_null($key)) return $array = $value;
-	$keys = explode(DIRECTORY_SEPARATOR, ltrim($key,DIRECTORY_SEPARATOR));
+	$keys = explode(DIRECTORY_SEPARATOR, ltrim($key, DIRECTORY_SEPARATOR));
 	while (count($keys) > 1) {
 		$key = array_shift($keys);
 		if ( ! isset($array[$key]) || ! is_array($array[$key])) {
@@ -54,9 +59,14 @@ function process_partition( $value, $name=null ) {
 <style>
 body {
 	font-family: "Helvetica Neue",Helvetica,Arial,sans-serif;
+	margin: 0;
+	padding: 0;
+}
+
+#container {
+	width: 1024px;
 	margin: auto;
 	position: relative;
-	width: 1024px;
 }
 
 h1 {
@@ -87,7 +97,7 @@ p.capitalize {
 
 .tab label {
 	background: #eee;
-	padding: 10px;
+	padding: 10px 12px;
 	border: 1px solid #ccc;
 	margin-left: -1px;
 	position: relative;
@@ -99,7 +109,7 @@ p.capitalize {
 }
 
 .tab th, .tab td {
-	padding: 6px 10px
+	padding: 8px 12px;
 }
 
 .content {
@@ -107,15 +117,14 @@ p.capitalize {
 	top: 28px;
 	left: 0;
 	background: white;
-	padding: 20px;
 	border: 1px solid #ccc;
-	height: 500px;
-	width: 560px;
+	height: 450px;
+	width: 100%;
 	overflow: auto;
 }
 
 .content table {
-	width: 100%
+	width: 100%;
 }
 
 .content th, .tab:nth-child(3) td {
@@ -171,13 +180,33 @@ p.capitalize {
 
 #partition {
 	position: absolute;
-	top: 650px;
-	font-size: 11px;
-	padding-bottom: 20px;
+	width: 100%;
+	height: 100%;
+	z-index: 10;
+	top: 0;
+	left: 0;
+	background: #ddd;
+	display: none;
+}
+
+#close-partition {
+	display: none;
+	position: absolute;
+	z-index: 20;
+	right: 15px;
+	top: 15px;
+	background: #f9373d;
+	color: #fff;
+	padding: 12px 15px;
+}
+
+#close-partition:hover {
+	background: #D32F33;
+	cursor: pointer;
 }
 
 #partition rect {
-	stroke: #eee;
+	stroke: #fff;
 	fill: #aaa;
 	fill-opacity: 1;
 }
@@ -191,8 +220,9 @@ p.capitalize {
 	pointer-events: none;
 }
 </style>
-<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/d3/3.0.1/d3.v3.min.js"></script>
-<script type="text/javascript">
+<script src="//cdnjs.cloudflare.com/ajax/libs/d3/3.0.1/d3.v3.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script>
 var hidden = {};
 function toggleVisible(head, row) {
 	if (!hidden[row]) {
@@ -211,136 +241,146 @@ function toggleVisible(head, row) {
 </head>
 
 <body>
-	<h1><?php echo $title; ?></h1>
+	<div id="container">
 
-	<div class="tabs">
+		<h1><?php echo $title; ?></h1>
 
-		<div class="tab">
-			<input type="radio" id="tab-status" name="tab-group-1" checked>
-			<label for="tab-status">Status</label>
-			<div class="content">
-				<table>
-					<?php
-					foreach ($status as $key => $value) {
-						if ($key === 'scripts') continue;
+		<div class="tabs">
 
-						if (is_array($value)) {
-							foreach ($value as $k => $v) {
-								if ($v === false) $value = 'false';
-								if ($v === true) $value = 'true';
-								if ($k === 'used_memory' || $k === 'free_memory' || $k  ===  'wasted_memory') $v = size_for_humans($v);
-								if ($k === 'current_wasted_percentage' || $k === 'opcache_hit_rate') $v = number_format($v,2) . '%';
-								if ($k === 'blacklist_miss_ratio') $v = number_format($v, 2) . '%';
-								if ($k === 'start_time' || $k === 'last_restart_time') $v = ($v ? date(DATE_RFC822, $v) : 'never');
+			<div class="tab">
+				<input type="radio" id="tab-status" name="tab-group-1" checked>
+				<label for="tab-status">Status</label>
+				<div class="content">
+					<table>
+						<?php
+						foreach ($status as $key => $value) {
+							if ($key === 'scripts') continue;
 
-								echo "<tr><th>$k</th><td>$v</td></tr>\n";
+							if (is_array($value)) {
+								foreach ($value as $k => $v) {
+									if ($v === false) $value = 'false';
+									if ($v === true) $value = 'true';
+									if ($k === 'used_memory' || $k === 'free_memory' || $k  ===  'wasted_memory') $v = size_for_humans($v);
+									if ($k === 'current_wasted_percentage' || $k === 'opcache_hit_rate') $v = number_format($v,2) . '%';
+									if ($k === 'blacklist_miss_ratio') $v = number_format($v, 2) . '%';
+									if ($k === 'start_time' || $k === 'last_restart_time') $v = ($v ? date(DATE_RFC822, $v) : 'never');
+
+									echo "<tr><th>$k</th><td>$v</td></tr>\n";
+								}
+								continue;
 							}
-							continue;
+							if ($value===false) $value = 'false';
+							if ($value===true) $value = 'true';
+							echo "<tr><th>$key</th><td>$value</td></tr>\n";
 						}
-						if ($value===false) $value = 'false';
-						if ($value===true) $value = 'true';
-						echo "<tr><th>$key</th><td>$value</td></tr>\n";
-					}
-					?>
-				</table>
+						?>
+					</table>
+				</div>
 			</div>
+
+			<div class="tab">
+				<input type="radio" id="tab-config" name="tab-group-1">
+				<label for="tab-config">Configuration</label>
+				<div class="content">
+					<table>
+						<?php
+						foreach ($config['directives'] as $key => $value) {
+							if ($value === false) $value = 'false';
+							if ($value === true) $value = 'true';
+							if ($key == 'opcache.memory_consumption') $value = size_for_humans($value);
+							echo "<tr><th>$key</th><td>$value</td></tr>\n";
+						}
+						?>
+					</table>
+				</div>
+			</div>
+
+			<div class="tab">
+				<input type="radio" id="tab-scripts" name="tab-group-1">
+				<label for="tab-scripts">Scripts (<?php echo count($status["scripts"]); ?>)</label>
+				<div class="content">
+					<table style="font-size:0.8em;">
+						<tr>
+							<th width="10%">Hits</th>
+							<th width="20%">Memory</th>
+							<th width="70%">Path</th>
+						</tr>
+						<?php
+						$d3scripts = $dirs = array();
+						foreach ($status['scripts'] as $key=>$data) {
+							$dirs[dirname($key)][basename($key)]=$data;
+							array_pset($d3scripts, $key, array(
+								'name' => basename($key),
+								'size' => $data['memory_consumption'],
+							));
+						}
+
+						asort($dirs);
+
+						$basename = '';
+						while (true) {
+							if (count($d3scripts) !=1) break;
+							$basename .= DIRECTORY_SEPARATOR . key($d3scripts);
+							$d3scripts = reset($d3scripts);
+						}
+
+						$d3scripts = process_partition($d3scripts, $basename);
+						$id = 1;
+
+						foreach ($dirs as $dir => $files) {
+							$count = count($files);
+							$file_plural = $count > 1 ? 's' : null;
+							$m = 0;
+							foreach ($files as $file => $data) {
+								$m += $data["memory_consumption"];
+							}
+							$m = size_for_humans($m);
+
+							if ($count > 1) {
+								echo '<tr>';
+								echo "<th class=\"clickable\" id=\"head-{$id}\" colspan=\"3\" onclick=\"toggleVisible('#head-{$id}', '#row-{$id}')\">{$dir} ({$count} file{$file_plural}, {$m})</th>";
+								echo '</tr>';
+							}
+
+							foreach ($files as $file => $data) {
+								echo "<tr id=\"row-{$id}\">";
+								echo "<td>{$data["hits"]}</td>";
+								echo "<td>" .size_for_humans($data["memory_consumption"]). "</td>";
+								echo $count > 1 ? "<td>{$file}</td>" : "<td>{$dir}/{$file}</td>";
+								echo '</tr>';
+							}
+
+							++$id;
+						}
+						?>
+					</table>
+				</div>
+			</div>
+
+			<div class="tab">
+				<input type="radio" id="tab-visualise" name="tab-group-1">
+				<label for="tab-visualise">Visualise Partition</label>
+				<div class="content"></div>
+			</div>
+
 		</div>
 
-		<div class="tab">
-			<input type="radio" id="tab-config" name="tab-group-1">
-			<label for="tab-config">Configuration</label>
-			<div class="content">
-				<table>
-					<?php
-					foreach ($config['directives'] as $key => $value) {
-						if ($value === false) $value = 'false';
-						if ($value === true) $value = 'true';
-						if ($key == 'opcache.memory_consumption') $value = size_for_humans($value);
-						echo "<tr><th>$key</th><td>$value</td></tr>\n";
-					}
-					?>
-				</table>
-			</div>
+		<div id="graph">
+			<form>
+				<label><input type="radio" name="dataset" value="memory" checked> Memory</label>
+				<label><input type="radio" name="dataset" value="keys"> Keys</label>
+				<label><input type="radio" name="dataset" value="hits"> Hits</label>
+			</form>
+
+			<div id="stats"></div>
 		</div>
 
-		<div class="tab">
-			<input type="radio" id="tab-scripts" name="tab-group-1">
-			<label for="tab-scripts">Scripts (<?php echo count($status["scripts"]); ?>)</label>
-			<div class="content">
-				<table style="font-size:0.8em;">
-					<tr>
-						<th width="10%">Hits</th>
-						<th width="20%">Memory</th>
-						<th width="70%">Path</th>
-					</tr>
-					<?php
-					$d3scripts = $dirs = array();
-					foreach($status['scripts'] as $key=>$data) {
-						$dirs[dirname($key)][basename($key)]=$data;
-						array_pset($d3scripts, $key, array(
-							'name' => basename($key),
-							'size' => $data['memory_consumption'],
-						));
-					}
+	</div><!-- end #container -->
 
-					asort($dirs);
-
-					$basename = '';
-					while(true) {
-						if( count($d3scripts)!=1 ) break;
-						$basename .= DIRECTORY_SEPARATOR . key($d3scripts);
-						$d3scripts = reset($d3scripts);
-					}
-
-					$d3scripts = process_partition($d3scripts, $basename);
-
-					$id = 1;
-
-					foreach($dirs as $dir => $files) {
-						$count = count($files);
-						$file_plural = $count > 1 ? 's' : null;
-						$m = 0;
-						foreach ($files as $file => $data) {
-							$m += $data["memory_consumption"];
-						}
-						$m = size_for_humans($m);
-
-						if ($count > 1) {
-							echo '<tr>';
-							echo "<th class=\"clickable\" id=\"head-{$id}\" colspan=\"3\" onclick=\"toggleVisible('#head-{$id}', '#row-{$id}')\">{$dir} ({$count} file{$file_plural}, {$m})</th>";
-							echo '</tr>';
-						}
-
-						foreach ($files as $file => $data) {
-							echo "<tr id=\"row-{$id}\">";
-							echo "<td>{$data["hits"]}</td>";
-							echo "<td>" .size_for_humans($data["memory_consumption"]). "</td>";
-							echo $count > 1 ? "<td>{$file}</td>" : "<td>{$dir}/{$file}</td>";
-							echo '</tr>';
-						}
-
-						++$id;
-					}
-					?>
-				</table>
-			</div>
-		</div>
-
-	</div>
-
-	<div id="graph">
-		<form>
-			<label><input type="radio" name="dataset" value="memory" checked> Memory</label>
-			<label><input type="radio" name="dataset" value="keys"> Keys</label>
-			<label><input type="radio" name="dataset" value="hits"> Hits</label>
-		</form>
-
-		<div id="stats"></div>
-	</div>
-
+	<div id="close-partition">&#10006; Close Visualisation</div>
 	<div id="partition"></div>
 
-	<script type="text/javascript">
+	<script>
 	<?php
 	$mem = $status['memory_usage'];
 	$stats = $status['opcache_statistics'];
@@ -357,9 +397,10 @@ function toggleVisible(head, row) {
 			height = 400,
 			radius = Math.min(width, height) / 2,
 			colours = ['#B41F1F', '#1FB437', '#ff7f0e'];
-	d3.scale.customColours = function() {
-		return d3.scale.ordinal().range(colours);
-	};
+			d3.scale.customColours = function() {
+				return d3.scale.ordinal().range(colours);
+			};
+
 	var colour = d3.scale.customColours();
 	var pie = d3.layout.pie()
 			.sort(null);
@@ -384,19 +425,19 @@ function toggleVisible(head, row) {
 	set_text("memory");
 
 	function set_text(t) {
-		if(t=="memory") {
+		if (t === "memory") {
 			d3.select("#stats").html(
 				"<table><tr><th style='background:#B41F1F;'>Used</th><td><?php echo size_for_humans($mem['used_memory'])?></td></tr>"+
 				"<tr><th style='background:#1FB437;'>Free</th><td><?php echo size_for_humans($mem['free_memory'])?></td></tr>"+
 				"<tr><th style='background:#ff7f0e;' rowspan=\"2\">Wasted</th><td><?php echo size_for_humans($mem['wasted_memory'])?></td></tr>"+
 				"<tr><td><?php echo number_format($mem['current_wasted_percentage'],2)?>%</td></tr></table>"
 			);
-		} else if(t=="keys") {
+		} else if (t === "keys") {
 			d3.select("#stats").html(
 				"<table><tr><th style='background:#B41F1F;'>Cached keys</th><td>"+dataset[t][0]+"</td></tr>"+
 				"<tr><th style='background:#1FB437;'>Free Keys</th><td>"+dataset[t][1]+"</td></tr></table>"
 			);
-		} else if(t=="hits") {
+		} else if (t === "hits") {
 			d3.select("#stats").html(
 				"<table><tr><th style='background:#B41F1F;'>Misses</th><td>"+dataset[t][0]+"</td></tr>"+
 				"<tr><th style='background:#1FB437;'>Cache Hits</th><td>"+dataset[t][1]+"</td></tr></table>"
@@ -405,10 +446,13 @@ function toggleVisible(head, row) {
 	}
 
 	function change() {
-		path = path.data(pie(dataset[this.value])); // update the data
-		path.transition().duration(750).attrTween("d", arcTween); // redraw the arcs
-		set_text(this.value);
+		if (typeof dataset[this.value] !== 'undefined') {
+			path = path.data(pie(dataset[this.value])); // update the data
+			path.transition().duration(750).attrTween("d", arcTween); // redraw the arcs
+			set_text(this.value);
+		}
 	}
+
 	function arcTween(a) {
 		var i = d3.interpolate(this._current, a);
 		this._current = i(0);
@@ -416,6 +460,7 @@ function toggleVisible(head, row) {
 			return arc(i(t));
 		};
 	}
+
 	function size_for_humans(bytes) {
 		if (bytes > 1048576) {
 				return (bytes/1048576).toFixed(2) + ' MB';
@@ -424,15 +469,15 @@ function toggleVisible(head, row) {
 		} else return bytes + ' bytes';
 	}
 
-	var w = 1024,
-			h = 600,
+	var w = window.innerWidth,
+			h = window.innerHeight,
 			x = d3.scale.linear().range([0, w]),
 			y = d3.scale.linear().range([0, h]);
 
 	var vis = d3.select("#partition")
 			.style("width", w + "px")
 			.style("height", h + "px")
-		.append("svg:svg")
+			.append("svg:svg")
 			.attr("width", w)
 			.attr("height", h);
 
@@ -443,7 +488,7 @@ function toggleVisible(head, row) {
 
 	var g = vis.selectAll("g")
 			.data(partition.nodes(root))
-		.enter().append("svg:g")
+			.enter().append("svg:g")
 			.attr("transform", function(d) { return "translate(" + x(d.y) + "," + y(d.x) + ")"; })
 			.on("click", click);
 
@@ -490,6 +535,36 @@ function toggleVisible(head, row) {
 	function transform(d) {
 		return "translate(8," + d.dx * ky / 2 + ")";
 	}
+
+	$(document).ready(function() {
+
+		function handleVisualisationToggle(close) {
+
+			$('#partition, #close-partition').fadeToggle();
+
+			// Is the visualisation being closed? If so show the status tab again
+			if (close) {
+
+				$('#tab-visualise').removeAttr('checked');
+				$('#tab-status').trigger('click');
+
+			}
+
+		}
+
+		$('label[for="tab-visualise"], #close-partition').on('click', function() {
+
+			handleVisualisationToggle(($(this).attr('id') === 'close-partition'));
+
+		});
+
+		$(document).keyup(function(e) {
+
+			if (e.keyCode == 27) handleVisualisationToggle(true);
+
+		});
+
+	});
 
 	</script>
 </body>
