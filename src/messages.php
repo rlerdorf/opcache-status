aa301237e69c524c8733f59d6d0d912fd9941dcc
Lewis Cowles <lewiscowles@me.com>
2015-01-16 03:27:26 +0000

    Update messages.php

--- src/messages.php
+++ src/messages.php
@@ -1,5 +1,6 @@
 <?php
 
-namespace Messages {
-  const ERR_NO_ZEND_APC = 'You do not have the Zend OPcache extension loaded, sample data is being shown instead.';
-}
+namespace lerdorf\opcode\messages;
+
+const ERR_NO_ZEND_APC = 'You do not have the Zend OPcache extension loaded, sample data is being shown instead.';
+
