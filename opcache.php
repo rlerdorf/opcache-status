b918e31004a47a4c7d8f0c14c62fa5464cda63e1
Lewis Cowles <lewiscowles@me.com>
2015-01-16 03:47:43 +0000

    Update opcache.php

--- opcache.php
+++ opcache.php
@@ -1,9 +1,11 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors',1);
 require_once __DIR__.'/vendor/autoload.php';
 
-use lerdorf\opcache\opCacheDataModel;
+use lerdorf\opcache\OpCacheDataModel;
 
-$dataModel = new OpCacheDataModel();
+$dataModel = new lerdorf\opcache\OpCacheDataModel();
 ?>
 <!DOCTYPE html>
 <html>
